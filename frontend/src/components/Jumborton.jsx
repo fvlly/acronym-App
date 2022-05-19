158a5ab87d9493ff6be71f7aaf306d3abc8e7dc0
Fvlly <luki0000001@gmail.com>
2022-05-19 19:10:34 +0100

    created design system

--- frontend/src/components/Jumborton.jsx
+++ frontend/src/components/Jumborton.jsx
@@ -1,6 +1,6 @@
 export default function Jumborton() {
   return (
-    <div className="jumborton">
+    <div className="jumborton bg-dark text-light">
       <h1> Found what you are looking for ?</h1>
       <p>Search through over 2.5 million acronymns</p>
       <p>See what others are currently searching </p>
