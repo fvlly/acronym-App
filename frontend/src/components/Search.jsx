5b633a5be2ebafd159c0072a65760da5ae85b7da
Fvlly <luki0000001@gmail.com>
2022-05-26 21:31:44 +0100

    added fetch functionality

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,13 +1,79 @@
+import { useEffect, useState, } from "react";
+import axios from "axios";
 import Loader from "./Loader";
 
+
+
+
 export default function Search() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [debouncedTerm,setDebouncedTerm]=useState(searchTerm)
+
+  useEffect(()=>{
+    const timerId = setTimeout(()=>{
+      setDebouncedTerm(searchTerm)
+    },600)
+
+    return ()=>{
+      clearTimeout(timerId)
+    }
+  },[searchTerm])
+
+  useEffect(()=>{
+    const search = async() =>{
+      const baseURL ='https://www.stands4.com/services/v2/abbr.php'
+      // const result = await axios.get(baseURL,{
+      //   headers:{
+      //     'Content-Type': 'application/json',
+      //   },
+      //   proxy:createProxyMiddleware({target:baseURL,changeOrigin:true}),
+      //   params:{
+      //     uid: '10423',
+      //     tokenid:'SQi5YYHnp7iOrycg',
+      //     term:debouncedTerm,
+      //     format: 'json',
+      //   },
+  
+      // })
+      const result = await axios({
+        method: 'get',
+        url: baseURL,
+        headers:{
+              'Content-Type': 'application/json',
+        },
+        params:{
+              uid: '10423',
+              tokenid:'SQi5YYHnp7iOrycg',
+              term:debouncedTerm,
+              format: 'json',
+            },
+
+
+      })
+
+      console.log(result);
+    }
+
+    if(debouncedTerm) {
+      search()
+    }
+  },[debouncedTerm])
+
   return (
     <>
       <div className="search-container">
         <div class="ui form">
           <div class="field">
             <label>Search Acronym</label>
-            <input type="text" placeholder="eg. WHO" />
+            <input
+              value={searchTerm}
+              onChange={(e) => {
+                setSearchTerm(e.target.value);
+                console.log(searchTerm);
+              }}
+              type="text"
+              placeholder="eg. WHO"
+            />
           </div>
           <div class="inline fields">
             <label htmlFor="category">Search by category:</label>
